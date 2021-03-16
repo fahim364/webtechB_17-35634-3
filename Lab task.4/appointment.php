44bba61d3b47ea9ff054a43889e984bc5347efd6
Fahim Farajannat <57459204+fahim364@users.noreply.github.com>
2021-03-16 20:06:35 +0600

    Add files via upload

---
+++ Lab task.4/appointment.php
@@ -0,0 +1,68 @@
+
+<?php
+   include('include/dnav.php');
+  
+?>
+
+
+
+
+
+  <!DOCTYPE html>
+  <html>
+  <head>
+  	<meta charset="utf-8">
+  	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+  	<title>Dashbord</title>
+  	
+  	<style type="text/css" media="screen">
+  		.*{
+  			padding: 0px;
+  			margin: 0px;
+  		}
+  		.text{
+  			font-family: "Montserrat",sans-serif; 
+  			padding-left: 40px;
+  		}
+
+      .mainbody{
+       
+        margin-left: 200px;
+        color: black;
+        padding: 10px;
+
+
+          
+       
+      }
+  		
+
+      
+
+      
+     
+  	</style>
+
+
+
+  </head>
+  <body>
+
+<!-- ======================== Start include side bar====================== -->
+  <div align="left" class="sidebar">
+  	<?php 
+        include('include/sidebar.php');
+     ?>
+  </div>
+ <!-- ========================End include side bar====================== -->
+
+ <!-- ========================Start Dashbord main body====================== -->
+<div align="center" class="mainbody">
+
+  <h1>Appointment list</h1>
+
+  <br>
+  <br>
+
+  <table border="1">
+    
